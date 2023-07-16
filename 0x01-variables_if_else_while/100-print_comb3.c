a032ac100928b452c49dea2f3754dd55cde48d17
Avumile <Smithavery435@gmail.com>
2023-07-16 09:44:57 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+*main - prints all possible combinations of two digits
+*Return: 0
+*/
+int main(void)
+{
+int i, j;
+
+for (i = 0; i <= 9; i++)
+{
+for (j = i + 1; j <= 9; j++)
+{
+putchar(i + '0');
+putchar(j + '0');
+
+if (i < 8 || j < 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
