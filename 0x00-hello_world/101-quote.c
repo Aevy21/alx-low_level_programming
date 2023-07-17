ce38b80fc957b8716c42e401bb4c08a4fcc81fda
Avumile <Smithavery435@gmail.com>
2023-07-16 17:14:07 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,14 @@
 #include <unistd.h>
+
 /**
-* main - Prints out the last part of a quote in the standard error.
-* Return: 1 if success.
+* main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+*followed by a new line, to standard error.
+*
+* Return: Always 1.
 */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
 return (1);
 }
