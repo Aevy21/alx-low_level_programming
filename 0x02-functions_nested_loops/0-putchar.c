4686cac379bd9cb44af1a9aff6d79bddba5dc3ba
Avumile <Smithavery435@gmail.com>
2023-07-18 04:28:16 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,8 @@
 #include "main.h"
  
 /**
-* main -  prints _putchar, followed by a new line
-* Return: Always 0 (Success)
+*main -  prints _putchar, followed by a new line
+*Return: Always 0 (Success)
 */
 int main(void)
 {
