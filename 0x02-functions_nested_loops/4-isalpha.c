c5824ff731c8f533b536120ddf3647a438c48fbc
Avumile <Smithavery435@gmail.com>
2023-07-18 06:15:04 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
 
 /**
 * _isalpha - checks for alphabetic character
-* c: the character to be checked
+*@c: the character to be checked
 * Return: 1 if c is a letter, 0 otherwise
 */
 
@@ -10,8 +10,8 @@ int _isalpha(int c)
 
 {
 if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-  return (1);
- else
-   return (0)
+return (1);
+else
+return (0);
 
 }
