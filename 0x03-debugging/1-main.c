bd1b42741dd8fc235018a35e290600ad17d3a056
Avumile <Smithavery435@gmail.com>
2023-07-18 17:39:54 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,14 +11,14 @@ int i;
 
 printf("Infinite loop incoming :(\n");
 
-/**
-* i = 0;
-*while (i < 10)
-*{
-*
-*putchar(i);
-*}
-*/
+i = 0;
+/*while loop starts here */
+while (i < 10)
+{
+putchar(i);
+}
+/* while loop ends here */
 printf("Infinite loop avoided! \\o/\n");
+
 return (0);
 }
