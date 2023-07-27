ed032b89b3d1fa82caaaa72996b75f048aa9e8b3
Avumile <Smithavery435@gmail.com>
2023-07-27 07:55:09 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 void print_array(int *a, int n);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
