70fe21c518e1725e0795e07f29cebe625d9577d4
Avumile <Smithavery435@gmail.com>
2023-07-31 08:59:12 -0700

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -4,7 +4,7 @@
 * _strpbrk - Searches a string for any of a set of bytes.
 * @s: The string to be searched.
 * @accept: The set of bytes to search for
-* Returns: a pointer to the byte in 's' otherwise NULL is returned.
+* Returns:a pointer to the byte in 's' otherwise NULL is returned.
 */
 
 
@@ -15,11 +15,12 @@ int i, j;
 for (i = 0; s[i] != '\0'; i++)
 {
 for (j = 0; accept[j] != '\0' && s[i] != accept[j]; j++)
-{j++;
+{
+j++;
 }
 if (accept[j] != '\0')
 {
-return (&(s[i]);
+return (s + i);
 }
 }
 return (s);
