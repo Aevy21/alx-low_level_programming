7fe40ae7554a85a9211cfe9d6daf411f72cfe5aa
Avumile <Smithavery435@gmail.com>
2023-08-02 11:52:54 -0700

    101-wildcmp.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int wildcmp(char *s1, char *s2);
+int is_palindrome(char *s);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
