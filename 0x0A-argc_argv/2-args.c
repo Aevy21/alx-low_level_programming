bb47b47d2c34626f19d262d7d35cb9fe34398ae6
Avumile <Smithavery435@gmail.com>
2023-08-04 04:09:56 -0700

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - prints all arguments it receives
+* @argc: number of arguments
+* @argv: array of argument
+* Return: Always 0 (Success)
+*/
+int main(int argc, char *argv[])
+
+{
+int i;
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
+}
