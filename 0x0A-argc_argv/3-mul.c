013fad25d1101cda404940880753072e8a056d7b
Avumile <Smithavery435@gmail.com>
2023-08-04 06:05:50 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,12 +13,12 @@ int main(int argc, char *argv[])
 {
 int a = 0;
 int b = 0;
-int result =0;
+int result = 0;
 if (argc == 3)
 {
 a = atoi(argv[1]);
 b = atoi(argv[2]);
-result = a * b;
+result = a *b;
 printf("%d\n", result);
 }
 else
