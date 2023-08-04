c5d6af9cdeb46c3df4f68932b7c2f476c829d80e
Avumile <Smithavery435@gmail.com>
2023-08-04 16:53:30 -0700

    statics

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,13 @@
 #include "main.h"
+/**
+* main - Entry point of the program
+*
+* Return: Always 0 (Success)
+*/
 
 int main(void)
 {
-_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+_puts("At the end of the day, my goal was to be the best hacker");
+_puts("\n\t- Kevin Mitnick");
 return (0);
 }
