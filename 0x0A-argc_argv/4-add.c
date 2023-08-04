06a54b386c16e03e7ce68d6a54d93d84b1a7f2bb
Avumile <Smithavery435@gmail.com>
2023-08-04 07:09:05 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,30 +1,64 @@
-#include <stdlib.h>
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 /**
- * main - Function that adds two numbers provided as command-line arguments.
- * @argc: Number of command-line arguments
- * @argv: Array of strings containing command-line arguments.
- * Return: 0 if successful, 1 if there is an error.
+ * check_num - check - string there are digit
+ * @str: array str
+ *
+ * Return: Always 0 (Success)
  */
+
+int check_num(char *str)
+{
+unsigned int count;
+
+count = 0;
+while (count < strlen(str))
+{
+if (!isdigit(str[count]))
+{
+return (0);
+}
+count++;
+}
+return (1);
+}
+/**
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return: Always 0 (Success)
+ */
+
+
 int main(int argc, char *argv[])
 {
-int a = 0;
-int b = 0;
-int result = 0;
+int count;
+int str_to_int;
+int sum = 0;
 
-if (argc == 3)
+count = 1;
+while (count < argc)
 {
-a = atoi(argv[1]);
-b = atoi(argv[2]);
-result = a + b;
-printf("%d\n", result);
+if (check_num(argv[count]))
+{
+str_to_int = atoi(argv[count]);
+sum += str_to_int;
 }
 else
 {
 printf("Error\n");
 return (1);
 }
+count++;
+}
+{
+printf("%d\n", sum);
 return (0);
 }
+}
+
+
 
