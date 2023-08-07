045f50bf5c87c46749f45f8a9b8329ca696e2672
Avumile <Smithavery435@gmail.com>
2023-08-07 12:31:42 -0700

    betty error

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,11 +2,13 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- * create_array - Creates an array of char and initializes it with a specific char.
+ * create_array - Creates an array of char and
+ * initializes it with a specific char.
  * @size: The size of the array.
  * @c: The char to initialize the array with.
  *
- * Return: On success, returns a pointer to the newly created array. Otherwise, returns NULL.
+ * Return: On success, returns a pointer to the newly created array.
+ * Otherwise, returns NULL.
  */
 char *create_array(unsigned int size, char c)
 {
