af56608b183e1f66cda535adfa96b9058d749753
Avumile <Smithavery435@gmail.com>
2023-08-07 13:02:52 -0700

    still null error

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,13 +14,16 @@ char *create_array(unsigned int size, char c)
 {
 unsigned int i;
 char *array = (char *)malloc(size * sizeof(char));
+if (size == 0)
+
+return (NULL);
 
 if (array == NULL)
+
 return (NULL);
 
 for (i = 0; i < size; i++)
 array[i] = c;
+
 return (array);
-if (size == 0)
-return (NULL);
 }
