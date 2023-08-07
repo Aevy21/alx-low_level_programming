ecb55f7fbdd45c3fbff0e2e364efed68a38fdc2c
Avumile <Smithavery435@gmail.com>
2023-08-07 15:06:10 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,13 +13,13 @@
 char *_strdup(char *str)
 {
 char *s;
+if (str == NULL)
+return (NULL);
+
 s = (char *)malloc(strlen(str) + 1);
-if (s == 0)
-return (NULL);
-if (s == NULL)
-return (NULL);
 strcpy(s, str);
 return (s);
-
+if (s == NULL)
+return (NULL);
 free(s);
 }
