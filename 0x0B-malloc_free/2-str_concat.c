4f223ce7bd0546d617fb8c1042463f970f38bd6c
Avumile <Smithavery435@gmail.com>
2023-08-09 09:53:56 -0700

    error

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,44 +1,50 @@
-
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 /**
- *str_concat- Combines two strings into a new dynamically allocated buffer.
+ *str_concat- Combines two strings into a  *new dynamically allocated buffer.
  * @s1: The first input string.
  * @s2: The second input string.
  *
- * Return: A dynamically allocated buffer containing the combined string,
+ * Return: A dynamically allocated buffer 
+ *containing the combined string,
  *or NULL if memory allocation fails.
  */
 
 char *str_concat(char *s1, char *s2)
 {
-    int s1_length = strlen(s1);
-    int s2_length = strlen(s2);
-    int size =  1 + s2_length + s1_length;
-    
-    char *s = malloc(size * sizeof(char));
+int j = 0;
+int i = 0;
+int len_s1 = strlen(s1);
+int len_s2 = strlen(s2);
+int size =  1 + len_s2+ len_s1;
+ 
+char *s = (char *)malloc(size + 1 * sizeof(char));
     if (s == NULL)
     {
-        return (NULL);
+	 return (NULL);
     }
-    
-    if (s1 != NULL)
-    {
-      while (s1 != '\0')
-    {
-        s++ = s1++;
-	}
+
+    if (s1 == NULL || s1 == 0)
+      {
+	return (NULL);
+      }
+    if (s2 == NULL || s2 == 0)
+      {
+	return (NULL);
+
+      }
+    for (; i < len_s1; ++i)
+      {
+        s[i] = s1[i];
     }
-    if (s2 != NULL)
-    {
-        while (s2 != '\0')
-    {
-        s++ = s2++;
+    for (; j < len_s2; ++j, ++i)
+      {
+        s[i] = s2[j];
     }
-    }
-    s = '\0';
+
+    s[i] = '\0';
     return (s);
 }
