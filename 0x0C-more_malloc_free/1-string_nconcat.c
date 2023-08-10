62805aef9337d9e7bc3f1859cfd227f1195566fa
Avumile <Smithavery435@gmail.com>
2023-08-10 13:25:01 -0700

    cat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -4,32 +4,46 @@
 #include <string.h>
 
 /**
- *str_concat- Combines two strings into a  *new dynamically allocated buffer.
- * @s1: The first input string.
- * @s2: The second input string.
- * 
-* Return: A dynamically allocated buffer
- *containing the combined string,
- *or NULL if memory allocation fails.
+ * *string_nconcat - concatenates n bytes of a string to another string
+ * @s1: string to append to
+ * @s2: string to concatenate from
+ * @n: number of bytes from s2 to concatenate to s1
+ *
+ * Return: pointer to the resulting string
  */
-char *str_concat(char *s1, char *s2)
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-int j = 0;
-int i = 0;
-int len_s1 = strlen(s1);
-int len_s2 = strlen(s2);
-int size = len_s1 + n + 1;
-char *s;
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
-while (s1[i] != '\0')
-i++;
-while (s2[j] != '\0')
-ci++;
+	char *s;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
 
-S[i] = '\0';
-return (S);
+	if (n < len2)
+		s = malloc(sizeof(char) * (len1 + n + 1));
+	else
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
+
+	if (!s)
+		return (NULL);
+
+	while (i < len1)
+	{
+		s[i] = s1[i];
+		i++;
+	}
+
+	while (n < len2 && i < (len1 + n))
+		s[i++] = s2[j++];
+
+	while (n >= len2 && i < (len1 + len2))
+		s[i++] = s2[j++];
+
+	s[i] = '\0';
+
+	return (s);
 }
+
