3bc6421dbea69073cc5db675d5eb34c15f270056
Avumile <Smithavery435@gmail.com>
2023-08-10 13:44:31 -0700

    err

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,7 +1,5 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include "main.h"
 
 /**
  * *_memset - fills memory with a constant byte
@@ -24,23 +22,25 @@ char *_memset(char *s, char b, unsigned int n)
 }
 
 /**
- * _calloc - create array of size size and assign char c
- * @size: size of array
- * @nmemb:array element
- * Return: returns NULL if If malloc fails.
+ * *_calloc - allocates memory for an array
+ * @nmemb: number of elements in the array
+ * @size: size of each element
+ *
+ * Return: pointer to allocated memory
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
-
 {
-char *array;
-unsigned int i;
+	char *ptr;
 
-if (nmemb == 0 || size == 0)
-return (NULL);
-array = calloc(sizeof(*array), size);
-if (size == 0 || array == NULL)
-return (NULL);
-for (i = 0; i < size; i++)
-array[i] = nmemb;
-return (array);
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(size * nmemb);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	_memset(ptr, 0, nmemb * size);
+
+	return (ptr);
 }
