f741f84f9207fb7946d05986165bd2ae0388e4b0
Avumile <avumileee@gmail.com>
2023-08-14 12:42:02 -0700

    file

--- 0x0D-preprocessor/02-main.c
+++ 0x0D-preprocessor/02-main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
-#define PRINT_FILENAME\
-       	printf("%s\n", __FILE__)
-
-int main() 
+/**
+ * main - prints the name of the file
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-    PRINT_FILENAME;
-    return (0);
+	printf("%s\n", __FILE__);
+	return (0);
 }
-
