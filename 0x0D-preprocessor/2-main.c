588b8d12dd399248bb0b33720d89bb0bef3d99e1
Avumile Ndlovu <Smithavery435@gmail.com>
2023-08-14 13:18:00 +0000

    filename

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 
-#define PRINT_FILENAME\
+#define  PRINT_FILENAME\
        	printf("%s\n", __FILE__)
+/**
+ *main - Entry point of the program
+ *
+ *Return: Always 0 (Success)
+ */
 
-int main() 
+int main(void) 
 {
-    PRINT_FILENAME;
-    return (0);
+PRINT_FILENAME;
+return (0);
 }
+
 
