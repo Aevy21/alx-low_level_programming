7cc2d9c7c034ebe84662f88f695de1d53109a79d
Avumile <avumileee@gmail.com>
2023-08-15 12:44:22 -0700

    Write a function that frees dogs.

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,18 +1,20 @@
 #include <stdlib.h>
 #include "dog.h"
-
 /**
-* free_dog - frees memory allocated for a struct dog
-* @d: struct dog to free
-*/
-
+ * free_dog - frees a dog
+ *
+ * @d: dog to free
+ *
+ * Return: void
+ */
 void free_dog(dog_t *d)
 {
-if (d)
-{
-free(d->name);
-free(d->owner);
-free(d);
-}
+	if (d == 0)
+		return;
+	if (d->name)
+		free(d->name);
+	if (d->owner)
+		free(d->owner);
+	free(d);
 }
 
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,6 +18,7 @@ struct dog
 */
 typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
