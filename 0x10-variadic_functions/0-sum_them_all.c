2302ef9db180f2c5cabcbb87c2582d6c8395d256
Avumile <avumileee@gmail.com>
2023-08-17 04:01:52 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,17 +10,17 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-        va_list args;
-        unsigned int i, sum = 0;
-        va_start(args, n);
-	if(n == 0)
-	{ 
-		return(0);
-	}
+va_list args;
+unsigned int i, sum = 0;
+va_start(args, n);
+if (n == 0)
+{
+return (0);
+}
 
-        for (i = 0; i < n; i++)
-                sum += va_arg(args, int);
+for (i = 0; i < n; i++)
+sum += va_arg(args, int);
 
-        va_end(args);
-        return (sum);
+va_end(args);
+return (sum);
 }
