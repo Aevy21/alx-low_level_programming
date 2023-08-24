9ff69191c8e76ea355afbfe685c0b04941aa54a9
Avumile Ndlovu <avumileee@gmail.com>
2023-08-24 03:27:23 -0700

    Write a function that returns the number of elements in a linked list_t list.

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,22 +9,15 @@
 size_t print_list(const list_t *h)
 {
 	size_t node_count = 0;
-
-	while (h != NULL)
+	while (h)
 	{
-		if (h->str != NULL)
-		{
-			printf("%s\n", h->str);
-		}
-		else
-		{
+		if (!h->str )
 			printf("[0] (nil)\n");
-		}
-
+		else
+			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
 		node_count++;
 	}
-
 	return (node_count);
 }
 
