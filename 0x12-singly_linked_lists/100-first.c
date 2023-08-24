4c37bec524b21235e63451a615e460e76e881266
Avumile Ndlovu <avumileee@gmail.com>
2023-08-24 09:47:52 -0700

    100-first.c file of a program with a func5ion tha% executes first

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,8 +4,9 @@
  * print_sent - prints a sentence before the main
  * function is executed
  */
-void print_sent()__attribute__((constructor));
-void print_sent()
+void print_sent(void)__attribute__((constructor));
+void print_sent(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+		printf("I bore my house upon my back!\n");
 }
