021496414aa74d05b60877a22c3de71878493e4c
Avumile Ndlovu <avumileee@gmail.com>
2023-08-24 03:30:17 -0700

    betty error on file

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,9 +9,10 @@
 size_t print_list(const list_t *h)
 {
 	size_t node_count = 0;
+	
 	while (h)
 	{
-		if (!h->str )
+		if (!h->str)
 			printf("[0] (nil)\n");
 		else
 			printf("[%u] %s\n", h->len, h->str);
