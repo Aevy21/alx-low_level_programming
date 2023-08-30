427d9084b4dbac3e8e0be4f3f104218563c2231a
Avumile Ndlovu <avumileee@gmail.com>
2023-08-29 22:37:01 -0700

    betty errors

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 char *argstostr(int ac, char **av);
+char **strtow(char *str);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
