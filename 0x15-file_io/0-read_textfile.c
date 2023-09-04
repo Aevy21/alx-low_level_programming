ec25815a410e70d57c00efe3fb3335fd6d038feb
Avumile Ndlovu <avumileee@gmail.com>
2023-09-04 06:56:01 -0700

    first firs in file handling

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,57 @@
+#include "main.h"
+
+/**
+ * read_textfile - Reads a text file and prints it to POSIX standard output.
+ * @filename: The name of the file to read.
+ * @letters: The number of letters to read and print.
+ * Return: The actual number of letters read and printed. 0 on failure.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file_descriptor;
+	char *buffer;
+	ssize_t bytes_read;
+	ssize_t num_letters;
+
+	if (filename == NULL)
+	{
+		return (0); 
+	}
+
+	file_descriptor = open(filename, O_RDONLY);
+	if (file_descriptor == -1)
+	{
+		return (0);
+	}
+
+	buffer = (char *)malloc(letters);
+	if (buffer == NULL)
+	{
+		close(file_descriptor);
+		return (0);
+	}
+
+	bytes_read = read(file_descriptor, buffer, letters);
+	if (bytes_read == -1)
+	{
+		free(buffer);
+		close(file_descriptor);
+		return (0);
+	}
+
+	num_letters= write(STDOUT_FILENO, buffer, bytes_read);
+	if (num_letters!= bytes_read)
+	{
+		free(buffer);
+		close(file_descriptor);
+		return (0);
+	}
+
+	free(buffer);
+	close(file_descriptor);
+
+	return (num_letters);
+}
+
+
