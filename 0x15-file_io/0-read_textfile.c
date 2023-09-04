e8b37f6532852ed6689b388410f49617adfdfc5a
Avumile Ndlovu <avumileee@gmail.com>
2023-09-04 07:07:00 -0700

    first firs in file handling

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,7 +6,6 @@
  * @letters: The number of letters to read and print.
  * Return: The actual number of letters read and printed. 0 on failure.
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int file_descriptor;
@@ -16,22 +15,19 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 	{
-		return (0); 
+		return (0);
 	}
-
 	file_descriptor = open(filename, O_RDONLY);
 	if (file_descriptor == -1)
 	{
 		return (0);
 	}
-
 	buffer = (char *)malloc(letters);
 	if (buffer == NULL)
 	{
 		close(file_descriptor);
 		return (0);
 	}
-
 	bytes_read = read(file_descriptor, buffer, letters);
 	if (bytes_read == -1)
 	{
@@ -39,18 +35,15 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(file_descriptor);
 		return (0);
 	}
-
-	num_letters= write(STDOUT_FILENO, buffer, bytes_read);
-	if (num_letters!= bytes_read)
+	num_letters = write(STDOUT_FILENO, buffer, bytes_read);
+	if (num_letters != bytes_read)
 	{
 		free(buffer);
 		close(file_descriptor);
 		return (0);
 	}
-
 	free(buffer);
 	close(file_descriptor);
-
 	return (num_letters);
 }
 
