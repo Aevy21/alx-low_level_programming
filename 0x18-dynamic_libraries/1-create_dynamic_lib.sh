1287a2fc6bf6907c0f3b4d968ad21a3fefd35d46
Avumile <Smithavery435@gmail.com>
2023-11-13 03:44:18 -0800

    script to creare dynamic libraries

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
-
-# Compile all .c files in the current directory and create a dynamic library named liball.so
-
-# Compile each .c file into object files
-for file in *.c; do
-    gcc -Wall -fPIC -c "$file" -o "${file%.c}.o"
-done
-
-# Create the dynamic library
+gcc -c -fpic *.c
 gcc -shared -o liball.so *.o
-
-# Clean up object files
-rm *.o
-
-echo "Dynamic library liball.so created successfully."
-
