f41ed54aeb0b50a3a254f11a37b65247826468d3
Avumile <smithavery435@gmail.com>
2023-11-30 22:45:36 +0200

    creating a hash table using linked list chain method '

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,42 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates a hash table
+ * @size: The size of the array
+ *
+ * Return: A pointer to the newly created hash table, or NULL if something went wrong
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    hash_table_t *new_table = NULL;
+    unsigned long int i;
+
+    if (size < 1)
+        return (NULL);
+
+    /* Allocate memory for the hash table structure */
+    new_table = malloc(sizeof(hash_table_t));
+    if (new_table == NULL)
+        return (NULL);
+    
+  /* Allocate memory for the array of pointers */
+    new_table->array = malloc(sizeof(hash_node_t *) * size);
+    if (new_table->array == NULL)
+    {
+        free(new_table);
+        return (NULL);
+    }
+
+    new_table->size = size;
+
+
+    /* Initialize each element of the array to NULL */
+    for (i = 0; i < size; i++)
+    {
+        new_table->array[i] = NULL;
+    }
+    
+    
+    return (new_table);
+}
