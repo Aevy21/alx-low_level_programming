cfbc4db2e34fc08932682615e349f5f690ab78c4
Avumile <smithavery435@gmail.com>
2023-11-30 23:07:11 +0200

    betty error

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -4,39 +4,36 @@
  * hash_table_create - Creates a hash table
  * @size: The size of the array
  *
- * Return: A pointer to the newly created hash table, or NULL if something went wrong
+ * Return: A pointer to the newly created hash table, or NULL on error
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *new_table = NULL;
-    unsigned long int i;
-
-    if (size < 1)
-        return (NULL);
-
-    /* Allocate memory for the hash table structure */
-    new_table = malloc(sizeof(hash_table_t));
-    if (new_table == NULL)
-        return (NULL);
-    
-  /* Allocate memory for the array of pointers */
-    new_table->array = malloc(sizeof(hash_node_t *) * size);
-    if (new_table->array == NULL)
-    {
-        free(new_table);
-        return (NULL);
-    }
-
-    new_table->size = size;
+	hash_table_t *new_table = NULL;
+	unsigned long int i;
 
 
-    /* Initialize each element of the array to NULL */
-    for (i = 0; i < size; i++)
-    {
-        new_table->array[i] = NULL;
-    }
-    
-    
-    return (new_table);
+	/* Allocate memory for the hash table structure */
+	new_table = malloc(sizeof(hash_table_t));
+	if (new_table == NULL)
+		return (NULL);
+
+	/* Allocate memory for the array of pointers */
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (new_table->array == NULL)
+	{
+		free(new_table);
+		return (NULL);
+	}
+
+	/* Initialize each element of the array to NULL */
+	for (i = 0; i < size; i++)
+	{
+		new_table->array[i] = NULL;
+	}
+
+	new_table->size = size;
+
+	return (new_table);
 }
+
