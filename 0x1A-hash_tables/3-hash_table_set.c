b41a9e051ce1da9b8bdbfae3651933ee8f3f6e55
Avumile <smithavery435@gmail.com>
2023-12-01 00:19:01 +0200

    adding an element to to the hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,6 +1,37 @@
 #include "hash_tables.h"
 
 /**
+ * create_node - Creates a new hash node
+ * @key: The key
+ * @value: The value
+ *
+ * Return: A pointer to the new hash node, or NULL on failure
+ */
+hash_node_t *create_node(const char *key, const char *value)
+{
+	hash_node_t *new_node = malloc(sizeof(hash_node_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->value = strdup(value);
+	if (new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (NULL);
+	}
+	new_node->next = NULL;
+	return (new_node);
+}
+/**
  * hash_table_set - Adds an element to the hash table
  * @ht: The hash table
  * @key: The key
