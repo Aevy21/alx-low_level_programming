7ff85dc2e8c493ab58b0e5ce3fa8eca385a1eb11
Avumile <smithavery435@gmail.com>
2023-12-01 13:28:39 +0200

    correction on adding an element logic

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,6 +1,5 @@
 #include "hash_tables.h"
 
-
 /**
  * hash_table_set - Adds an element to the hash table
  * @ht: The hash table
@@ -11,41 +10,61 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index;
-	hash_node_t *new_node;
-	/* Check for invalid parameters */
-	if (ht == NULL || key == NULL || *key == '\0')
-		return (0);
+    unsigned long int index;
+    hash_node_t *new_node;
+    hash_node_t *temp;
 
-	/* Calculate the index using the hash function */
-	index = hash_djb2((unsigned char *)key) % ht->size;
+    /* Check for invalid parameters */
+    if (ht == NULL || key == NULL || *key == '\0')
+        return (0);
 
-	/* Create a new hash node */
-	new_node = malloc(sizeof(hash_node_t));
-	if (new_node == NULL)
-		return (0);
+    /* Calculate the index using the hash function */
+    index = hash_djb2((unsigned char *)key) % ht->size;
 
-	/* Duplicate the key */
-	new_node->key = strdup(key);
-	if (new_node->key == NULL)
-	{
-		free(new_node);
-		return (0);
-	}
+    /* Check for an existing key in the linked list */
+    temp = ht->array[index];
+    while (temp != NULL)
+    {
+        if (strcmp(temp->key, key) == 0)
+        {
+            /* Key already exists, update its value */
+            free(temp->value);
+            temp->value = strdup(value);
+            if (temp->value == NULL)
+            {
+                return (0); /* Memory allocation failed */
+            }
+            return 1; /* Update successful */
+        }
+        temp = temp->next;
+    }
 
-	/* Duplicate the value */
-	new_node->value = strdup(value);
-	if (new_node->value == NULL)
-	{
-		free(new_node->key);
-		free(new_node);
-		return (0);
-	}
+    /* If key doesn't exist, create a new node and add it at the beginning */
+    new_node = malloc(sizeof(hash_node_t));
+    if (new_node == NULL)
+        return (0);
 
+    /* Duplicate the key */
+    new_node->key = strdup(key);
+    if (new_node->key == NULL)
+    {
+        free(new_node);
+        return (0);
+    }
 
-	/* Collision: Add new node at the beginning of the list */
-	new_node->next = ht->array[index];
-	ht->array[index] = new_node;
+    /* Duplicate the value */
+    new_node->value = strdup(value);
+    if (new_node->value == NULL)
+    {
+        free(new_node->key);
+        free(new_node);
+        return (0);
+    }
 
-	return (1); /* Operation succeeded */
+    /* Collision: Add new node at the beginning of the list */
+    new_node->next = ht->array[index];
+    ht->array[index] = new_node;
+
+    return (1); /* Operation succeeded */
 }
+
