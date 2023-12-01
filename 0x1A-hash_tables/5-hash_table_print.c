045b9a482d775f96488aa71c235d11aedd6746b5
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2023-12-01 14:29:19 +0200

    Update 5-hash_table_print.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -29,7 +29,7 @@ void hash_table_print(const hash_table_t *ht)
 			current = current->next;
 		}if (i < ht->size - 1 && ht->array[i + 1] != NULL) {
             printf(", ");
-			
+		}	
 	}
 	printf("}\n");
 }
