5f2efbec7eb29ec9bd19df3efdc5bbbe47e76bbc
Avumile <smithavery435@gmail.com>
2023-12-01 12:58:55 +0200

    correction on adding an element logic

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,36 +1,6 @@
 #include "hash_tables.h"
 
-/**
- * create_node - Creates a new hash node
- * @key: The key
- * @value: The value
- *
- * Return: A pointer to the new hash node, or NULL on failure
- */
-hash_node_t *create_node(const char *key, const char *value)
-{
-	hash_node_t *new_node = malloc(sizeof(hash_node_t));
 
-	if (new_node == NULL)
-		return (NULL);
-
-	new_node->key = strdup(key);
-	if (new_node->key == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-
-	new_node->value = strdup(value);
-	if (new_node->value == NULL)
-	{
-		free(new_node->key);
-		free(new_node);
-		return (NULL);
-	}
-	new_node->next = NULL;
-	return (new_node);
-}
 /**
  * hash_table_set - Adds an element to the hash table
  * @ht: The hash table
@@ -41,18 +11,41 @@ hash_node_t *create_node(const char *key, const char *value)
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	unsigned long int index;
+	hash_node_t *new_node;
+	/* Check for invalid parameters */
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (0);
 
-	unsigned long int index = hash_djb2((unsigned char *)key) % ht->size;
-	hash_node_t *new_node = create_node(key, value);
+	/* Calculate the index using the hash function */
+	index = hash_djb2((unsigned char *)key) % ht->size;
 
+	/* Create a new hash node */
+	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
+
+	/* Duplicate the key */
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (0);
+	}
+
+	/* Duplicate the value */
+	new_node->value = strdup(value);
+	if (new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (0);
+	}
+
 
 	/* Collision: Add new node at the beginning of the list */
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 
-	return (1);
+	return (1); /* Operation succeeded */
 }
